ac2fbdf5b521eca6c9e4b05dbb96718d0d2d1060
Mary Enn <rodary782@gmail.com>
2024-03-13 12:37:29 +0300

    домашняя задача с лекции

---
+++ lec1/homework1/Program.cs
@@ -0,0 +1,17 @@
+﻿//вычислить значение формулы a*b / c+d дробь, где а b c d 
+// это некоторые целые числа.
+//вывести результат на экран.
+
+int a = 14;
+int b = 15;
+double sum1 = a * b;
+
+int c = 13;
+int d = 54;
+double sum2 = c + d;
+
+double sum = sum1 / sum2;
+
+Console.WriteLine(sum1);
+Console.WriteLine(sum2);
+Console.WriteLine(sum);
