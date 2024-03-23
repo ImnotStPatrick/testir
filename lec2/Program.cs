6cbc6d9d02a4e7af9910dfd40856ec483620d299
Mary Enn <rodary782@gmail.com>
2024-03-23 19:28:54 +0300

    lectoin2

--- lec2/Program.cs
+++ lec2/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿int[] arr = new int[5]; // создали массив на 5 элементов
+arr[0] = 45; 
+arr[1] = 56;
+arr[2] = 1;
+arr[3] = 323;
+arr[4] = 23754; // заполнили элементы массива случайными целыми числами
+
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]); // выводим элементы массива на экран
