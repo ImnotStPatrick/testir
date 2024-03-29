e5b09bc3fe0fca9927b039bdce424696db3db5f6
Mary Enn <rodary782@gmail.com>
2024-03-29 11:27:05 +0300

    4 семинар

--- sem4/Program.cs
+++ sem4/Program.cs
@@ -174,6 +174,7 @@ Console.WriteLine(str);
 */
 
 // решение препода
+/* 
 int[] RandomMass(int m)
 {
     int[] Number = new int[m];
@@ -205,6 +206,104 @@ int[] arr = RandomMass(n);
 int str = FindFirstAndLast(arr);
 Console.WriteLine();
 Console.WriteLine(str);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+using System;
+using System.Linq;
+
+//Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
+class UserInputToCompileForTest
+{ 
+    // Подсчет количества четных элементов массива
+    // numbers - массив, в котором ведется подсчет
+    public static int CountEvenItems(int[] numbers)
+    {
+        //Напишите свое решение здесь
+          int a = 0; 
+          foreach (int e in numbers)
+           {
+           if ( e % 2 == 0)
+           {
+            // Console.Write($"{e} ");
+            a++;
+           }
+    }
+    }
+    
+    public static void PrintResult(int[] array)
+    {
+        //Напишите свое решение здесь
+        PrintResult = Console.WriteLine(numbers);
+       
+    }
+}
+
+//Не удаляйте и не меняйте класс Answer!
+class Answer
+{
+    public static void Main(string[] args)
+    {
+        int[] array;
+        
+
+        if (args.Length >= 1) {
+            // Объединяем все аргументы командной строки в одну строку
+            string joinedArgs = string.Join(" ", args);
+
+            // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
+            array = joinedArgs.Split(", ")
+                                  .Select(int.Parse)
+                                  .ToArray();
+            
+            // Теперь arr содержит преобразованные в целые числа из командной строки
+        
+        } else {
+           // Если аргументов на входе нет
+            array = new int[] {2, 5, 2, 3, 6, 3, 7, 8, 2, 4}; // Создание массива
+        }
+        UserInputToCompileForTest.PrintResult(array);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
